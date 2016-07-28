c08af0b6d6cb0edb5456be8045b983346def12be
Ahmad Benmrad <batikhsouri@gmail.com>
2016-07-29 01:01:03 +0200

    Identation and what not

--- www/MiniSodium.js
+++ www/MiniSodium.js
@@ -54,31 +54,36 @@ var MiniSodium = {
 		cordova.exec(resultHandlerFactory(callback), callback, 'MiniSodium', 'crypto_secretbox_open_easy', params);
 	},
 	from_hex: function(str) {
-        if (!is_hex(str)) {
-            throw new TypeError("The provided string doesn't look like hex data");
-        }
-        var result = new Uint8Array(str.length / 2);
-        for (var i = 0; i < str.length; i += 2) {
-            result[i >>> 1] = parseInt(str.substr(i, 2), 16);
-        }
-        return result;
-    },
+    if (!is_hex(str)) {
+      throw new TypeError("The provided string doesn't look like hex data");
+    }
+    var result = new Uint8Array(str.length / 2);
+    for (var i = 0; i < str.length; i += 2) {
+      result[i >>> 1] = parseInt(str.substr(i, 2), 16);
+    }
+    return result;
+  },
 	to_hex: function(bytes) {
 		if (is_hex(bytes)) return bytes;
+		if (!(bytes instanceof Uint8Array)) throw new TypeError('bytes must be a Uint8Array instance');
 
-        var str = "", b, c, x;
-        for (var i = 0; i < bytes.length; i++) {
-            c = bytes[i] & 0xf;
-            b = bytes[i] >>> 4;
-            x = (87 + c + (((c - 10) >> 8) & ~38)) << 8 |
-                (87 + b + (((b - 10) >> 8) & ~38));
-            str += String.fromCharCode(x & 0xff) + String.fromCharCode(x >>> 8);
-        }
-        return str;
-    },
+    var str = "", b, c, x;
+    for (var i = 0; i < bytes.length; i++) {
+      c = bytes[i] & 0xf;
+      b = bytes[i] >>> 4;
+      x = (87 + c + (((c - 10) >> 8) & ~38)) << 8 |
+          (87 + b + (((b - 10) >> 8) & ~38));
+      str += String.fromCharCode(x & 0xff) + String.fromCharCode(x >>> 8);
+    }
+    return str;
+  },
 	is_hex: function(s){
-		return typeof s == 'string' && s.length % 2 == 0 && /^([a-f]|[0-9])+$/ig.test(s);
+		return typeof s === 'string' && s.length % 2 === 0 && /^([a-f]|[0-9])+$/ig.test(s);
 	}
 };
+
+var from_hex = MiniSodium.from_hex;
+var to_hex = MiniSodium.to_hex;
+var is_hex = MiniSodium.is_hex;
 
 module.exports = MiniSodium;
